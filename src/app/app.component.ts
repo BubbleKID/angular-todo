440d1cdfb2a521ac852e4702b07da54b8f9c389f
BubbleKID <fqchenxin@gmail.com>
2021-06-21 23:42:48 +1000

    add to do app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { faPlus, faTrashAlt, faCheck, faCircle } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-todo';
+  title = 'Todo List App';
+  faPlus = faPlus;
+  faTrashAlt = faTrashAlt;
+  faCheck = faCheck;
+  faCircle= faCircle;
+  filter: 'all' | 'active' | 'done' = 'all';
+  allItems = [
+    { description: 'eat', done: true },
+    { description: 'sleep', done: false },
+    { description: 'play', done: false },
+    { description: 'laugh', done: false },
+  ];
+
+  addItem(description:string) {
+    this.allItems.unshift({
+      description,
+      done: false
+    });
+  }
+
+  removeItem(index:number) {
+    this.allItems.splice(index, 1);
+  }
+
+  checkItem(index:number) {
+    this.allItems[index].done = !this.allItems[index].done;
+  }
+
+  get items() {
+    if (this.filter === 'all') {
+      return this.allItems;
+    }
+    return this.allItems.filter(item => this.filter === 'done' ? item.done : !item.done);
+  }
 }
