c81444ff7bc1779f7546ba86e11b3cfa2c506fac
BubbleKID <fqchenxin@gmail.com>
2021-06-23 21:27:59 +1000

    fixed test file

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-todo'`, () => {
+  it(`should have as title 'Todo List App'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-todo');
+    expect(app.title).toEqual('Todo List App');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angular-todo app is running!');
+    expect(compiled.querySelector('.todo-container h1').textContent).toContain('Todo List App');
   });
 });
